af4030383c43cd93497b9bf9049dacdf1114bcbe
Uri Rado <uregoteratest@gmail.com>
2023-06-19 18:30:15 +0300

    L97

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,44 @@
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+            
+        }
+        public async Task<IEnumerable<AppUser>> GetUserAsync()
+        {
+            return await _context.Users
+            .Include(p => p.Photos)
+            .ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(X => X.UserName == username);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
