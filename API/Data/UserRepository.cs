753c800ce54b471a9e48de58ba873272f780ed1c
Uri Rado <uregoteratest@gmail.com>
2023-06-19 18:58:50 +0300

    End of section 8

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,5 +1,8 @@
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
@@ -7,11 +10,30 @@ namespace API.Data
     public class UserRepository : IUserRepository
     {
         private readonly DataContext _context;
-        public UserRepository(DataContext context)
+
+        private readonly IMapper _mapper;
+        public UserRepository(DataContext context, IMapper mapper)
         {
+            _mapper = mapper;
             _context = context;
             
         }
+
+        public async Task<IEnumerable<MemberDto>> GetMemberAsync()
+        {
+            return await _context.Users
+              .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+              .ToListAsync();
+        }
+
+        public async Task<MemberDto> GetMemberAsync(string username)
+        {
+            return await _context.Users
+                .Where(x => x.UserName == username)
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
+        }
+
         public async Task<IEnumerable<AppUser>> GetUserAsync()
         {
             return await _context.Users
--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -1,3 +1,4 @@
+using API.DTOs;
 using API.Entities;
 
 namespace API.Interfaces
@@ -9,6 +10,8 @@ namespace API.Interfaces
         Task<IEnumerable<AppUser>> GetUserAsync();
         Task<AppUser> GetUserByIdAsync(int id);
         Task<AppUser> GetUserByUsernameAsync(string username);
+        Task<IEnumerable<MemberDto>> GetMemberAsync();
+        Task<MemberDto> GetMemberAsync(string username);
         
     }
 }
