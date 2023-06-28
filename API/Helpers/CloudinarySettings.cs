8a2f4074c08949e1fca637de7d3094b0d211851b
Uri Rado <uregoteratest@gmail.com>
2023-06-28 11:43:26 +0300

    l-128

---
+++ API/Helpers/CloudinarySettings.cs
@@ -0,0 +1,12 @@
+namespace API.Helpers
+{
+    public class CloudinarySettings
+    {
+        public string CludName { get; set; }
+
+        public string ApiKey { get; set; }
+
+        public string ApiSecret { get; set; }
+        
+    }
+}
