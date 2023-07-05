7193d0777d562bc094bfd6c7563adc3c017079a3
Uri Rado <uregoteratest@gmail.com>
2023-07-05 16:21:37 +0300

    L-154

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -19,11 +20,13 @@ namespace API.Data
             
         }
 
-        public async Task<IEnumerable<MemberDto>> GetMemberAsync()
+        public async Task<PagedList<MemberDto>> GetMemberAsync(UserParams userParams)
         {
-            return await _context.Users
+            var query=  _context.Users
               .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-              .ToListAsync();
+              .AsNoTracking();
+
+            return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<MemberDto> GetMemberAsync(string username)
