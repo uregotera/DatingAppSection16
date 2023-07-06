5b417d78004fad0a9326ff398c30f24a3a3d717f
Uri Rado <uregoteratest@gmail.com>
2023-07-06 12:05:15 +0300

    L-157

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -22,11 +22,15 @@ namespace API.Data
 
         public async Task<PagedList<MemberDto>> GetMemberAsync(UserParams userParams)
         {
-            var query=  _context.Users
-              .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-              .AsNoTracking();
+            var query=  _context.Users.AsQueryable();
+            query = query.Where(u => u.UserName != userParams.CurrentUsername);
+            query = query.Where(u => u.Gender == userParams.Gender);
+              
 
-            return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
+            return await PagedList<MemberDto>.CreateAsync(
+                query.AsNoTracking().ProjectTo<MemberDto> (_mapper.ConfigurationProvider),
+                userParams.PageNumber, 
+                userParams.PageSize);
         }
 
         public async Task<MemberDto> GetMemberAsync(string username)
