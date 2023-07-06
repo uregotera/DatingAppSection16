d82fb9458fd313af5ec4b99a9f90f669b4abc830
Uri Rado <uregoteratest@gmail.com>
2023-07-06 10:47:21 +0300

    L-156

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -35,6 +35,12 @@ export class MemberListComponent implements OnInit {
     })
   }
 
-  
+  pageChanged(event: any) {
+    if(this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
+    
+  }
 
 }
