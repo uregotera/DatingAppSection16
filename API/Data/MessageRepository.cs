c2244aaf66b973ad75aba1f9ee846d318d3dc5b7
Uri Rado <uregoteratest@gmail.com>
2023-07-13 10:46:51 +0300

    L-185

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -3,14 +3,18 @@ using API.DTOs;
 using API.Entities;
 using API.Helpers;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace API.Data
 {
     public class MessageRepository : IMessageRepository
     {
         private readonly DataContext _context;
-        public MessageRepository(DataContext context)
+        private readonly IMapper _mapper;
+        public MessageRepository(DataContext context, IMapper mapper)
         {
+            _mapper = mapper;
             _context = context;            
         }
         public void AddMessage(Message message)
@@ -28,9 +32,24 @@ namespace API.Data
             return await _context.Messages.FindAsync(id);
         }
 
-        public Task<PagedList<MessageDto>> GetMessagesForUser()
+        public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
         {
-            throw new NotImplementedException();
+             var query = _context.Messages
+                 .OrderByDescending(x => x.MessageSent)
+                 .AsQueryable();
+
+             query = messageParams.Container switch
+             {
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username),
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username),
+                _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.DateRead == null)
+             };
+
+             var messages =  query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
+
+             return await PagedList<MessageDto>
+             .CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
+
         }
 
         public Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId)
