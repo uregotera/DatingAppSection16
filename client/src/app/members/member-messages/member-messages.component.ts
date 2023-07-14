7c154c9781b8de2194f413a9b2ad0d1adca6d4c9
Uri Rado <uregoteratest@gmail.com>
2023-07-14 15:37:59 +0300

    L-191

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -8,21 +8,15 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
-  @Input() username?: string;
-  messages: Message[] = [];
+  @Input() username?: string; 
+  @Input() messages: Message[] = [];
 
   constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
-    this.loadMessages();
+    
   }
 
-  loadMessages() {
-    if (this.username) {
-      this.messageService.getMessageThread(this.username).subscribe({
-        next: messages => this.messages = messages
-      })
-    }
-  }
+  
 
 }
