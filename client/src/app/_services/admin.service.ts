e0051d1aa02adb42ac4cef2253b077b2c66feef5
Uri Rado <uregoteratest@gmail.com>
2023-07-31 15:08:58 +0300

    End of section 16

--- client/src/app/_services/admin.service.ts
+++ client/src/app/_services/admin.service.ts
@@ -14,4 +14,9 @@ export class AdminService {
   getUsersWithRoles() {
     return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles')
   }
+
+  updateUserRoles(username: string, roles: any) {
+    return this.http.post<string[]>(this.baseUrl + 'admin/edit-roles/'
+      + username + '?roles=' + roles, {});
+  }
 }
